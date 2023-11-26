04d9b13ed502c2deb117ce1f4c19a4034da2a571
nvatuu <tunv.todo@gmail.com>
2023-11-26 07:23:51 +0700

    selection sort - merge sorted array

---
+++ src/myutils/sorting/SelectionSort.java
@@ -0,0 +1,33 @@
+package myutils.sorting;
+
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/merge-sorted-array/
+ * Idea: Use treeset store all pushable stacks
+ */
+
+
+public class SelectionSort {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] arr1 = Arrays.copyOf(nums1, m);
+        int[] arr2 = Arrays.copyOf(nums2, n);
+
+        int i=0, j=0, k=0;
+        while (i < m && j < n) {
+            if (arr1[i] <= arr2[j]) {
+                nums1[k++] = arr1[i++];
+            }
+            else {
+                nums1[k++] = arr2[j++];
+            }
+        }
+        while (i < m) {
+            nums1[k++] = arr1[i++];
+        }
+        while (j < n) {
+            nums1[k++] = arr2[j++];
+        }
+    }
+}
